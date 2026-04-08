b3c173c0bb1a3c5f0481c513ec5b019ca5817525
liamzoro <liammzoro@proton.me>
2026-04-07 19:58:46 -0700

    Update blinken.py

--- cs2/blinken.py
+++ cs2/blinken.py
@@ -1,34 +1,143 @@
 from fltk import *
 import random
+import time
 
 class Blinken(Fl_Window) :
-    
+    masterSequence = []
+    userSequence = []
+    sequenceStarted = False
+    gameDifficulty = 0
+    patternAdditions = 1
+    allChosenButtons = []
+
     def __init__(self,width,height,title) :
         super().__init__(width,height,title)
-        self.begin()
-        self.buttonBackground = Fl_Box(height//8,height//8,width - (height//8)*2,height - (height//8)*2)
-        self.buttonBackground.box(FL_UP_BOX)
-        self.buttonYellow = Fl_Button(height//8,
-                                      height//8,
-                                      self.buttonBackground.w()//2,
-                                      self.buttonBackground.h()//2)
 
-        self.buttonRed = Fl_Button(height//8 + self.buttonBackground.w()//2,
-                                   height//8,
-                                   self.buttonBackground.w()//2,
-                                   self.buttonBackground.h()//2)
+        self.begin()
+        self.buttonBackground = Fl_Box(height//8,height//6,width - (height//8)*2,height - (height//8)*2)
+        self.buttonBackground.box(FL_FLAT_BOX)
+        self.buttonBackground.color(34)
+        self.buttonBackground.label("...")
+        self.buttonBackground.labelsize(height//8)
+        self.buttonBackground.labelcolor(7)
+        
+        self.buttonYellow = Fl_Button(height//8,
+                                      height//6,
+                                      self.buttonBackground.w()//2 - height//16,
+                                      self.buttonBackground.h()//2 - height//16,
+                                      "")
+        self.buttonYellow.color(94)
+
+        self.buttonRed = Fl_Button(height//8 + self.buttonBackground.w()//2 + height//16,
+                                   height//6,
+                                   self.buttonBackground.w()//2 - height//16,
+                                   self.buttonBackground.h()//2 - height//16,
+                                   " ")
+        self.buttonRed.color(80)
 
         self.buttonBlue = Fl_Button(height//8,
-                                   height//8 + self.buttonBackground.h()//2,
-                                   self.buttonBackground.w()//2,
-                                   self.buttonBackground.h()//2)
+                                    height//6 + self.buttonBackground.h()//2 + height//16,
+                                    self.buttonBackground.w()//2 - height//16,
+                                    self.buttonBackground.h()//2 - height//16,
+                                    "  ")
+        self.buttonBlue.color(176)
 
-        self.buttonGreen = Fl_Button(height//8 + self.buttonBackground.w()//2,
-                                     height//8 + self.buttonBackground.h()//2,
-                                     self.buttonBackground.w()//2,
-                                     self.buttonBackground.h()//2)
+        self.buttonGreen = Fl_Button(height//8 + self.buttonBackground.w()//2 + height//16,
+                                     height//6 + self.buttonBackground.h()//2 + height//16,
+                                     self.buttonBackground.w()//2 - height//16,
+                                     self.buttonBackground.h()//2 - height//16,
+                                     "   ")
+        self.buttonGreen.color(62)
+
+        self.buttons = [self.buttonYellow,self.buttonRed,self.buttonBlue,self.buttonGreen]
+        
+        for but in range(len(self.buttons)) :
+            self.buttons[but].callback(self.handleClick)
+            self.resizable(self.buttons[but])
+        self.resizable(self.buttonBackground)
+
+        self.gameOptions = Fl_Menu_Bar(0,0,width,24)
+        self.gameOptions.add("Begin",FL_ALT | ord("s"),self.beginSequence)
+        self.gameOptions.add("Difficulty/Normal",0,self.chooseDifficulty,0)
+        self.gameOptions.add("Difficulty/Hard",0,self.chooseDifficulty,1)
+        self.gameOptions.add("Difficulty/Extreme",0,self.chooseDifficulty,2)
+        self.gameOptions.box(FL_ENGRAVED_BOX)
+        self.gameOptions.color(0)
+        self.gameOptions.textcolor(7)
+
+        self.buttonColors = [FL_YELLOW,FL_RED,FL_BLUE,FL_GREEN,94,80,176,62]
+
         self.end()
 
-ligma = Blinken(812,256,"LirmaBlinken")
+    def handleClick(self,wid) :
+        if not Blinken.sequenceStarted :
+            return
+        elif len(wid.label()) == 3 :
+            Blinken.userSequence.append(3)
+        elif len(wid.label()) == 2 :
+            Blinken.userSequence.append(2)
+        elif len(wid.label()) == 1 :
+            Blinken.userSequence.append(1)
+        else :
+            Blinken.userSequence.append(0)
+
+        if Blinken.userSequence != Blinken.masterSequence[:len(Blinken.userSequence)] :
+            fl_message("Stinker input")
+            Blinken.masterSequence = []
+            Blinken.userSequence = []
+
+        elif len(Blinken.userSequence) == len(Blinken.masterSequence) :
+            Blinken.userSequence = []
+            Blinken.sequenceStarted = False
+            Fl.add_timeout(1.0,self.runSequenceLoop)
+
+
+    def chooseDifficulty(self,wid,difficulty) :
+        if difficulty == 2 :
+            Blinken.gameDifficulty = 2
+        if difficulty == 1 :
+            Blinken.gameDifficulty = 1
+        if difficulty == 0 :
+            Blinken.gameDifficulty = 0
+    
+    def beginSequence(self,wid) :
+        Blinken.masterSequence = []
+        if Blinken.gameDifficulty == 2 :
+            Blinken.patternAdditions = 4
+        elif Blinken.gameDifficulty == 1 :
+            Blinken.patternAdditions = 2
+        elif Blinken.gameDifficulty == 0 :
+            Blinken.patternAdditions = 1
+        
+        Fl.add_timeout(1.0,self.runSequenceLoop)
+
+    def runSequenceLoop(self) :
+        for i in range(len(Blinken.masterSequence) + Blinken.patternAdditions) :
+            time.sleep(0.5)
+            if i + 1 > len(Blinken.masterSequence) :
+                chosenButton = random.randrange(4)
+                Blinken.allChosenButtons.append(chosenButton)
+                Blinken.masterSequence.append(chosenButton)
+                print(chosenButton)
+            else :
+                chosenButton = Blinken.masterSequence[i]
+
+            self.buttons[chosenButton].color(self.buttonColors[chosenButton])
+            self.redraw()
+            Fl.flush()
+            Fl.check()
+            time.sleep(0.5)
+            self.buttons[chosenButton].color(self.buttonColors[chosenButton+4])
+            self.redraw()
+            Fl.flush()
+            Fl.check()
+            print("jibbit")
+
+        Blinken.sequenceStarted = True
+
+ligma = Blinken(512,256,"LirmaBlinken")
 ligma.show()
+
+#Fl.background(33,32,33)
+Fl.visible_focus(0)
 Fl.run()
